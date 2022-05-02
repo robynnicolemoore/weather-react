60082020161303473ede1d8f527ae0237f731ab5
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-05-01 21:53:35 -0400

    removed unnecessary imports

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 import "./Weather.css";
 import FormattedDate from "./FormattedDate";
