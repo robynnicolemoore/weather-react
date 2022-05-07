ed6e7776118da99664dfab5a21217ce79fc2593b
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-05-07 10:31:15 -0400

    Added API integration to forecast for everything but weather icon

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 import axios from "axios";
 
-function handleResponse(response) {
-  console.log(response.data);
-}
-
 export default function Forecast(props) {
-  const apiKey = "512fdc436870661a029a84285db6b3f9";
-  let latitude = props.coordinates.lon;
-  let longitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Forecast">
-      <h4>Monday</h4>
-      <ReactAnimatedWeather icon={"CLEAR_DAY"} color={"goldenrod"} size={20} />
-      <p>65°F | 80°F</p>
-      <h4>Tuesday</h4>
-      <ReactAnimatedWeather icon={"CLEAR_DAY"} color={"goldenrod"} size={20} />
-      <p>65°F | 80°F</p>
-      <h4>Wednesday</h4>
-      <ReactAnimatedWeather icon={"CLEAR_DAY"} color={"goldenrod"} size={20} />
-      <p>65°F | 80°F</p>
-      <h4>Thursday</h4>
-      <ReactAnimatedWeather icon={"CLEAR_DAY"} color={"goldenrod"} size={20} />
-      <p>65°F | 80°F</p>
-    </div>
-  );
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <h4>{forecast[1].dt}</h4>
+        <ReactAnimatedWeather
+          icon={"CLEAR_DAY"}
+          color={"goldenrod"}
+          size={20}
+        />
+        <p>
+          {forecast[1].temp.min}°F | {forecast[1].temp.max}°F
+        </p>
+        <h4>{forecast[2].dt}</h4>
+        <ReactAnimatedWeather
+          icon={"CLEAR_DAY"}
+          color={"goldenrod"}
+          size={20}
+        />
+        <p>
+          {forecast[2].temp.min}°F | {forecast[2].temp.max}°F
+        </p>
+        <h4>{forecast[3].dt}</h4>
+        <ReactAnimatedWeather
+          icon={"CLEAR_DAY"}
+          color={"goldenrod"}
+          size={20}
+        />
+        <p>
+          {forecast[3].temp.min}°F | {forecast[3].temp.max}°F
+        </p>
+        <h4>{forecast[4].dt}</h4>
+        <ReactAnimatedWeather
+          icon={"CLEAR_DAY"}
+          color={"goldenrod"}
+          size={20}
+        />
+        <p>
+          {forecast[4].temp.min}°F | {forecast[4].temp.max}°F
+        </p>
+      </div>
+    );
+  } else {
+    const apiKey = "c13ec1823489873786dad083e25adf72";
+    let latitude = props.coordinates.lon;
+    let longitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
