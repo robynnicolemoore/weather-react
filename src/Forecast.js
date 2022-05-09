7c3c4ce47d2b2d54271f91914e135621390d8188
robynnicolemoore <101610206+robynnicolemoore@users.noreply.github.com>
2022-05-08 21:42:51 -0400

    Adjusted forecast days

--- src/Forecast.js
+++ src/Forecast.js
@@ -22,7 +22,7 @@ export default function Forecast(props) {
         <div className="row">
           <div className="col">
             {forecast.map(function (dailyForecast, index) {
-              if (index >= 1 && index < 5) {
+              if (index >= 0 && index < 4) {
                 return (
                   <div className="col" key={index}>
                     <ForecastDay data={dailyForecast} />
